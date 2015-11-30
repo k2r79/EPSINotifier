b94f9c0c48db6aedc9a59f0c04fa6dcb927af145
Vincent Kelleher <k2r79@hotmail.fr>
2015-11-30 14:18:19 +0100

    Complete parser

--- parser.py
+++ parser.py
@@ -6,11 +6,11 @@ import datetime
 from Cours import Cours
 from pymongo import MongoClient
 
+auth = yaml.safe_load(open("auth.yml"))
+
 days = [ "103.1200", "122.5200", "141.9200", "161.3200", "180.7200" ]
 
 def parse_schedule(year, week_number):
-    auth = yaml.safe_load(open("auth.yml"))
-
     auth_session = requests.Session()
     auth_request = auth_session.post("http://ecampusbordeaux.epsi.fr/login_form", data={
         "form.submitted": "1",
@@ -33,7 +33,7 @@ def parse_schedule(year, week_number):
         matiere = cases[case_index].xpath('.//td[@class="TCase"]/text()')[0]
         prof = cases[case_index].xpath('.//td[@class="TCProf"]/text()[1]')[0]
         horaires = cases[case_index].xpath('.//td[@class="TChdeb"]/text()')[0]
-        salle = cases[case_index].xpath('.//td[@class="TCSalle"]/text()')[0]
+        salle = parse_salle(cases[case_index].xpath('.//td[@class="TCSalle"]/text()')[0])
 
         splitted_horaire = horaires.split(" - ")
         datetimes = [
@@ -45,18 +45,23 @@ def parse_schedule(year, week_number):
 
     return cours
 
+def parse_salle(salle_string):
+    salle_matcher = re.search(r'(F[0-9]+)', salle_string)
+
+    return salle_matcher.group(1)
+
 def parse_datetime(case, year, week_number, time):
-    case_matcher = re.match(re.compile(".*left:([0-9\.]*)"), case.xpath("./@style")[0])
+    case_matcher = re.search(r'.*left:([0-9\.]*)', case.xpath("./@style")[0])
     day = days.index(case_matcher.group(1)) + 1
 
     return datetime.datetime.strptime('%s-%s-%s-%s' % (year, week_number, day, time), "%Y-%W-%w-%H:%M")
 
-mongo_client = MongoClient(auth.database_url)
+mongo_client = MongoClient(auth["database_url"])
 mongo_db = mongo_client["cours"]
 mongo_cours = mongo_db["planning"]
 
 for cours in parse_schedule(2015, 48):
     print(cours.__dict__)
-    mongo_cours.insert(cours.__dict__)
+    #mongo_cours.insert(cours.__dict__)
 
 print("Schedule parsed and inserted !")
