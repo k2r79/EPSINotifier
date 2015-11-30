2ca2c2f3b51b8bd7177467b990fe4b438095ff31
Vincent Kelleher <k2r79@hotmail.fr>
2015-11-30 12:45:59 +0100

    Project

---
+++ Cours.py
@@ -0,0 +1,10 @@
+class Cours:
+    def __init__(self, matiere, prof, horaires, salle):
+        self.matiere = matiere
+        self.prof = prof
+        self.horaire_debut = horaires[0]
+        self.horaire_fin = horaires[1]
+        self.salle = salle
+
+    def __str__(self):
+        return "%s - %s - [%s - %s] - %s" % (self.matiere, self.prof, self.horaire_debut, self.horaire_fin, self.salle)
---
+++ parser.py
@@ -0,0 +1,62 @@
+import yaml
+import requests
+from lxml import html
+import re
+import datetime
+from Cours import Cours
+from pymongo import MongoClient
+
+days = [ "103.1200", "122.5200", "141.9200", "161.3200", "180.7200" ]
+
+def parse_schedule(year, week_number):
+    auth = yaml.safe_load(open("auth.yml"))
+
+    auth_session = requests.Session()
+    auth_request = auth_session.post("http://ecampusbordeaux.epsi.fr/login_form", data={
+        "form.submitted": "1",
+        "came_from": "http://ecampusbordeaux.epsi.fr/",
+        "js_enabled": "0",
+        "cookies_enabled": "",
+        "login_name": "",
+        "pwd_empty": "0",
+        "__ac_name": auth["username"],
+        "__ac_password": auth["password"],
+        "submit": "Se connecter"
+    })
+
+    schedule_request = auth_session.get("http://ecampusbordeaux.epsi.fr/emploi_du_temps")
+    schedule_tree = html.fromstring(schedule_request.content)
+
+    cours = []
+    cases = schedule_tree.xpath('//div[@class="Case" and not(@id="Apres")]')
+    for case_index in range(len(cases)):
+        matiere = cases[case_index].xpath('.//td[@class="TCase"]/text()')[0]
+        prof = cases[case_index].xpath('.//td[@class="TCProf"]/text()[1]')[0]
+        horaires = cases[case_index].xpath('.//td[@class="TChdeb"]/text()')[0]
+        salle = cases[case_index].xpath('.//td[@class="TCSalle"]/text()')[0]
+
+        splitted_horaire = horaires.split(" - ")
+        datetimes = [
+            parse_datetime(cases[case_index], year, week_number, splitted_horaire[0]),
+            parse_datetime(cases[case_index], year, week_number, splitted_horaire[1])
+        ]
+
+        cours.append(Cours(matiere, prof, datetimes, salle))
+
+    return cours
+
+def parse_datetime(case, year, week_number, time):
+    case_matcher = re.match(re.compile(".*left:([0-9\.]*)"), case.xpath("./@style")[0])
+    day = days.index(case_matcher.group(1)) + 1
+
+    return datetime.datetime.strptime('%s-%s-%s-%s' % (year, week_number, day, time), "%Y-%W-%w-%H:%M")
+
+mongo_client = MongoClient(auth.database_url)
+mongo_db = mongo_client["cours"]
+mongo_cours = mongo_db["planning"]
+
+for cours in parse_schedule(2015, 48):
+    print(cours.__dict__)
+    mongo_cours.insert(cours.__dict__)
+
+print("Schedule parsed and inserted !")
